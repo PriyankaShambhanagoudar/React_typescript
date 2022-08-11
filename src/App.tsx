bf90e2e147686192bd56d7ccb341e2b9717d6ff1
priyankashambhanagoudar <priyassss789@gmail.com>
2022-08-11 12:11:42 +0530

    components Create

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Todos from "./components/Todos";
 
 function App() {
- return <div>React with TypeScript</div>;
+    return (
+        <div>
+            React with TypeScript
+            <Todos />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Todos.tsx
@@ -0,0 +1,11 @@
+function Todos() {
+    return (
+        <div>
+            <ul>
+                <li>Learn React </li>
+                <li>Learn React-TypeScript </li>
+            </ul>
+        </div>
+    );
+}
+export default Todos;
